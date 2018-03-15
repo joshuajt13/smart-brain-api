5ea23e41c5a6279f837ab02f52d975e354872162
root <root@DESKTOP-3OID0FF.localdomain>
2018-03-14 21:48:44 -0400

    updating database

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
+    host : 'postgresql-lively-54612',
     user : 'postgres',
     password : 'postgres',
     database : 'smart-brain'
